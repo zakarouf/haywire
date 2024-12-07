c415e8f12b817f54ba5d08d08940064a1c725cac
zakarouf <zak.d.official@gmail.com>
2024-12-07 12:06:27 +0530

    Added: Fn VTable Table for FNINFO and default loader

--- src/hw_dev_types.c
+++ src/hw_dev_types.c
@@ -42,6 +42,18 @@ hw_Type *hw_TypeSys_set(hw_TypeSys *ts, hw_Type const *type)
     return dest;
 }
 
+hw_VarFn hw_Type_getvt(hw_Type const *T, char const *name, hw_uint name_size)
+{
+    for (size_t i = 0; i < T->vt_count; i++) {
+        if(T->vtinfo[i].name_size == name_size) {
+            if(!memcmp(T->vtinfo[i].name, name, name_size)) {
+                return T->vt[i];
+            }
+        }
+    }
+    return hw_VarFn_UNREACHABLE;
+}
+
 /**********************************************************************/
 /* All Types VTables:
  */
@@ -54,7 +66,7 @@ hw_Type *hw_TypeSys_set(hw_TypeSys *ts, hw_Type const *type)
       , hw_uint const count)
 
 #define _ALLOC(SIZE)            HW_TYPESYS_ALLOC(ts, SIZE)
-#define _REALLOC(PTR, SIZE)     HW_TYPESYS_REALLOC(ts,PTR, SIZE)
+#define _REALLOC(PTR, SIZE)     HW_TYPESYS_REALLOC(ts, PTR, SIZE)
 #define _FREE(PTR)              HW_TYPESYS_FREE(ts, PTR)
 
 #define _GET_SELF() (args[0])
@@ -100,7 +112,7 @@ static void *_loadfile(
     return data;
 }
 
-static DEFN(hw_unreachable_func) {
+DEFN(hw_VarFn_PANIC) {
     (void)ts;
     (void)args;
     (void)tid;
@@ -109,9 +121,9 @@ static DEFN(hw_unreachable_func) {
     return HW_VARP_NIL();
 }
 
-/**
- * Var List
- */
+/*******************
+ *    Var List
+ *******************/
 
 DEFN(hw_VarList_new) {
     (void)args;
@@ -175,12 +187,46 @@ DEFN(hw_VarList_push_shallow) {
     return HW_VARP_NIL();
 }
 
+
+DEFN(hw_VarList_push) {
+    (void)count;
+    (void)tid;
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+
+    _SELF_BIND(hw_VarList *, as_list);
+    if(self->len <= self->lenUsed) {
+        hw_Var expand_args[] = { _GET_SELF(), (hw_Var){.as_uint = self->len * 2} };
+        hw_byte expand_args_tids[] = { hw_TypeID_list, hw_TypeID_uint };
+        hw_VarList_expand(ts, expand_args, expand_args_tids, 2);
+        _GET_SELF() = expand_args[0];
+        self = _GET_SELF().as_list;
+    }
+    
+    self->data[self->lenUsed] = _GET_ARG_RAW(0);
+    self->lenUsed += 1;
+    
+    return HW_VARP_NIL();
+}
+
+
+DEFN(hw_VarList_pop) {
+    _SELF_BIND(hw_VarList *, as_list);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
+DEFN(hw_VarList_popStream) {
+    _SELF_BIND(hw_VarList *, as_list);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
 DEFN(hw_VarList_reserve) {
     _SELF_BIND(hw_VarList *, as_list);
     hw_uint size = _GET_ARG(0, as_uint);
     hw_uint avail = self->len - self->lenUsed;
     if(avail < size) { hw_VarList_expand(ts, args, tid, count); }
-    return HW_VARP_NIL();
+    return HW_VARP_NIL(); // expand will set list proper, so we dont.
 }
 
 DEFN(hw_VarList_delete) {
@@ -192,7 +238,8 @@ DEFN(hw_VarList_delete) {
     for (hw_uint i = 0; i < self->lenUsed; i++) {
         hw_byte const T = self->tid[i];
         if(ts->types[T].is_obj) {
-            HW_VAR_CALL_CORE(T, ts, vtcore.delete, self->data + i, self->tid + i, 1);
+            HW_DEBUG(HW_LOG("USING vt[2] here %s", ""););
+            HW_VAR_CALL_CORE(T, ts, vt[2], self->data + i, self->tid + i, 1);
         }
     }
     _FREE(self);
@@ -200,7 +247,9 @@ DEFN(hw_VarList_delete) {
 }
 
 
+/*******************
 // STRINGS
+ *******************/
 DEFN(hw_String_new) {
     (void)args;
     (void)tid;
@@ -290,22 +339,32 @@ DEFN(hw_String_append_cstr) {
 }
 
 DEFN(hw_String_newFrom_file) {
-    _SELF(hw_String *);
-    HW_ASSERT(0 && "NOT IMPLEMENTED");
-    hw_uint filesize = 0;
-    hw_ptr filedata = _loadfile(ts, "X", 1, &filesize);
-    if(filedata == NULL) {
+    HW_DEBUG(_CHECK_ARGS(2, hw_TypeID_string, hw_TypeID_string));
+
+    hw_String *path = _GET_ARG(1, as_string);
+    hw_Var filesize;
+    hw_Var filedata = { 
+        .as_ptr = _loadfile(
+                ts, (void const *)path->data
+                , path->lenUsed, &filesize.as_uint)
+    };
+    
+    hw_Var args_list[] = { {0}, filedata, filesize };
+    hw_byte tids_list[] = { 0, hw_TypeID_ptr, hw_TypeID_uint };
+    hw_String_newFrom_cstr(ts, args_list, tids_list, 3);
+
+    if(filedata.as_ptr == NULL) {
         return HW_VARP_ERROR(
-            &((hw_Error){.error = 0, .str = "FNF", .str_size = 3}));
+            &((hw_Error){.error = 0, .str = (void*)"FNF", .str_size = 3}));
     }
     
-    _SELF_ASSIGN(as_string);
+    _GET_SELF().as_string = args_list[0].as_string;
     return HW_VARP_NIL();
 }
 
-/**
- * VarArr
- */
+/*******************
+ *    Var Arr
+ *******************/
 DEFN(hw_VarArr_newFrom_conf) {
     (void)args;
     (void)tid;
@@ -336,8 +395,9 @@ DEFN(hw_VarArr_delete) {
 
     if (ts->types[self->tid].is_obj) {
         for (hw_uint i = 0; i < self->lenUsed; i++) {
+            HW_DEBUG(HW_LOG("USING vt[2] here %s", ""););
             HW_VAR_CALL_CORE(
-                self->tid, ts, vtcore.delete, self->data + i, &self->tid, 1);
+                self->tid, ts, vt[2], self->data + i, &self->tid, 1);
         }
     }
 
@@ -381,88 +441,227 @@ DEFN(hw_VarArr_pushStream) {
     return HW_VARP_NIL();
 }
 
+DEFN(hw_VarArr_pop) {
+    _SELF_BIND(hw_VarArr *, as_arr);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
+DEFN(hw_VarArr_popStream) {
+    _SELF_BIND(hw_VarArr *, as_arr);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
+/**************************
+ * Section: hw_VarTable
+ **************************/
+
+DEFN(hw_VarTable_new) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
+DEFN(hw_VarTable_delete) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+DEFN(hw_VarTable_set) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+DEFN(hw_VarTable_set_copy) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+DEFN(hw_VarTable_get) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+DEFN(hw_VarTable_get_index) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
+DEFN(hw_VarTable_set_index) {
+    _SELF_BIND(hw_VarTable *, as_table);
+    HW_ASSERT(0 && "NOT IMPLEMENTED");
+    return HW_VARP_NIL();
+}
+
+
 /*
  * Default TypeSys
  */
+#define FNINFO(T, _name, _argc, _mutc, ...)\
+    {{\
+        .name = (void *) #_name\
+      , .name_size = sizeof(#_name)-1\
+      , .arg_count = _argc+1\
+      , .mut_count = _mutc\
+      , .argT = (hw_byte[]){__VA_ARGS__, hw_TypeID_nil}\
+    }, T##_##_name} 
 
-hw_TypeSys* hw_TypeSys_default_with_allocator(hw_Allocator allocator)
+#define _TYPEVT_MAX 6
+struct {
+    hw_FnInfo info;
+    hw_VarFn  fn;
+} static TYPEVT[][_TYPEVT_MAX]= {
+
+    [hw_TypeID_string] = {
+        FNINFO(hw_String, new, 0, 0, hw_TypeID_string),
+        FNINFO(hw_String, delete, 0, 0, hw_TypeID_string),
+        FNINFO(hw_String, newFrom_cstr, 2, 0
+                , hw_TypeID_string, hw_TypeID_uint, hw_TypeID_ptr),
+    },
+
+    [hw_TypeID_arr] = {
+        FNINFO(hw_VarArr, newFrom_conf, 1, 0, hw_TypeID_arr, hw_TypeID_uint),
+        FNINFO(hw_VarArr, delete, 0, 0, hw_TypeID_arr),
+        FNINFO(hw_VarArr, push, 1, 0, hw_TypeID_arr, hw_TypeID_any),
+        FNINFO(hw_VarArr, pushStream, 2, 0, hw_TypeID_arr, hw_TypeID_uint, hw_TypeID_ptr),
+        FNINFO(hw_VarArr, pop, 0, 0, hw_TypeID_arr),
+        FNINFO(hw_VarArr, popStream, 1, 0, hw_TypeID_arr, hw_TypeID_uint),
+    },
+
+    [hw_TypeID_list] = {
+        FNINFO(hw_VarList, new, 0, 0, hw_TypeID_list),
+        FNINFO(hw_VarList, delete, 0, 0, hw_TypeID_list),
+        FNINFO(hw_VarList, push_shallow, 1, 0, hw_TypeID_list, hw_TypeID_any),
+        FNINFO(hw_VarList, push, 1, 0, hw_TypeID_list, hw_TypeID_any),
+        FNINFO(hw_VarList, pop, 0, 0, hw_TypeID_list),
+        FNINFO(hw_VarList, popStream, 1, 0, hw_TypeID_list, hw_TypeID_uint),
+    },
+
+    [hw_TypeID_table] = {
+        FNINFO(hw_VarTable, new, 0, 0, hw_TypeID_table),
+        FNINFO(hw_VarTable, delete, 0, 0, hw_TypeID_table),
+        FNINFO(hw_VarTable, get, 2, 0
+                , hw_TypeID_table, hw_TypeID_any, hw_TypeID_any),
+        FNINFO(hw_VarTable, set, 2, 0
+                , hw_TypeID_table, hw_TypeID_any, hw_TypeID_any),
+        FNINFO(hw_VarTable, get_index, 2, 1
+                , hw_TypeID_table, hw_TypeID_any, hw_TypeID_uint),
+        FNINFO(hw_VarTable, set_index, 2, 1
+                , hw_TypeID_table, hw_TypeID_any, hw_TypeID_uint)
+    },
+};
+
+
+static void _default_setall_atoms(hw_TypeSys *ts)
 {
-    HW_ASSERT(1); // Checking assert sanity
-                  //
-    hw_TypeSys *ts = hw_TypeSys_new(hw_TypeID_TOTAL, allocator);
-    HW_ASSERT(ts != NULL);
-    
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+    hw_Type *T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_nil
                     ,   .name = "nil"
                     ,   .name_size = 3
                     ,   .unitsize = 0
                 }));
+    HW_ASSERT(T);
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+    T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_ptr
                     ,   .name = "ptr"
                     ,   .name_size = 3
                     ,   .unitsize = sizeof(hw_ptr)
                 }));
+    HW_ASSERT(T);
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+    T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_int
                     ,   .name = "int"
                     ,   .name_size = 3
                     ,   .unitsize = sizeof(hw_int)
                 }));
+    HW_ASSERT(T);
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+    T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_uint
                     ,   .name = "uint"
                     ,   .name_size = 4
                     ,   .unitsize = sizeof(hw_uint)
                 }));
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+    T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_float
                     ,   .name = "float"
                     ,   .name_size = 5
                     ,   .unitsize = sizeof(hw_float)
                 }));
+    HW_ASSERT(T);
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
-                        .id = hw_TypeID_list
-                    ,   .is_obj = 1
-                    ,   .name = "list"
-                    ,   .name_size = 4
-                    ,   .unitsize = sizeof(hw_VarList)
-                    ,   .vtcore = {
-                            .new = hw_VarList_new
-                           ,.delete = hw_VarList_delete
-                        }
-                }));
+}
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+static void _default_setall_objects(hw_TypeSys *ts)
+{
+    hw_Type *T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_arr
                     ,   .is_obj = 1
                     ,   .name = "arr"
                     ,   .name_size = 3
                     ,   .unitsize = sizeof(hw_VarArr)
-                    ,   .vtcore = {
-                            .new = hw_unreachable_func
-                        ,   .delete = hw_VarArr_delete
-                    }
-                }));
+                    }));
+    HW_ASSERT(T);
 
-    HW_ASSERT(hw_TypeSys_set(ts, &(hw_Type){
+
+    T = (hw_TypeSys_set(ts, &(hw_Type){
+                        .id = hw_TypeID_list
+                    ,   .is_obj = 1
+                    ,   .name = "list"
+                    ,   .name_size = 4
+                    ,   .unitsize = sizeof(hw_VarList)
+                    }));
+    HW_ASSERT(T);
+
+    T = (hw_TypeSys_set(ts, &(hw_Type){
+                        .id = hw_TypeID_table
+                    ,   .is_obj = 1
+                    ,   .name = "table"
+                    ,   .name_size = 5
+                    ,   .unitsize = sizeof(hw_VarTable)
+                    }));
+    HW_ASSERT(T);
+
+
+    T = (hw_TypeSys_set(ts, &(hw_Type){
                         .id = hw_TypeID_string
                     ,   .is_obj = 1
                     ,   .name = "string"
                     ,   .name_size = 6
                     ,   .unitsize = sizeof(hw_String)
-                    ,   .vtcore = {
-                            .new = hw_String_new
-                           ,.delete = hw_String_delete
-                        }
-                }));
+                    }));
+    HW_ASSERT(T);
+}
 
+static void _default_vt_binds(hw_TypeSys *ts)
+{
+    for (size_t tid = 0; tid < hw_TypeID_TOTAL; tid++) {
+        hw_Type *T = ts->types + tid;
+        size_t vt_id = 0;
+        for (; vt_id < _TYPEVT_MAX; vt_id++) {
+            T->vt[tid] = TYPEVT[tid][vt_id].fn;
+            T->vtinfo[tid] = TYPEVT[tid][vt_id].info;
+            if (!TYPEVT[tid][vt_id].info.name_size) {
+                T->vt[vt_id] = hw_VarFn_UNREACHABLE;
+            }
+        }
+        T->vt_count = vt_id;
+    }
+}
+
+hw_TypeSys* hw_TypeSys_default_with_allocator(hw_Allocator allocator)
+{
+    hw_TypeSys *ts = hw_TypeSys_new(hw_TypeID_TOTAL, allocator);
+    HW_ASSERT(ts != NULL);
+    
+    _default_setall_atoms(ts);
+    _default_setall_objects(ts);
+    _default_vt_binds(ts);
     return ts;
 }
 
