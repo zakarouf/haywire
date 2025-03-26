6b96ff45389336933c424643606ec10d61253d7e
zakarouf <zak.d.official@gmail.com>
2025-03-26 22:44:00 +0530

    Added: Subprocess functions

--- src/core.c
+++ src/core.c
@@ -389,8 +389,131 @@ hw_String *hw_stripfile_path_ext(hw_State *hw, hw_byte const *file_name
 
 /**
  * Section: ALLOCATORS
- */
 
+/**
+ * Access: Command line & Sub-process
+ */
+#include <spawn.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+extern char **environ;
+char** hw_getenv(void)
+{
+    return environ;
+}
+
+int hw_fork(const char *command)
+{
+    pid_t child, p;
+    int   status;
+
+    /*
+     * Prepare pipes et cetera first.
+    */
+
+    /* Fork to create the subprocess. */
+    child = fork();
+    if (child == (pid_t)-1) {
+        /* Cannot fork(); usually out of resources (user limits).
+         * see errno for details. With <string.h>, you can use
+         * strerror(errno) to obtain the error string itself. */
+        return 0;
+
+    } else if (!child) {
+        /* This is the child process itself.
+         * Do whatever cleanup is necessary, then
+         * execute the subprocess command. */
+        execl("/bin/sh", "sh", "-c", command, NULL);
+
+        /* This is only reached if the exec failed;
+         * again, see errno for reason.
+         * Always have the child process exit! */
+        return 0;
+    }
+
+    /* This is only run by the parent process
+     * (because the child always exits within the
+     *  else if body above).
+     *
+     * The subprocess PID is 'child'.
+    */
+
+    /* Wait for the child process to exit. */
+    do {
+        status = 0;
+        p = waitpid(child, &status, 0);
+        if (p == (pid_t)-1 && errno != EINTR)
+            break; /* Error */
+    } while (p != child);
+
+    if (p != child) {
+        /* Child process was lost.
+         * If (p == (pid_t)-1), errno describes the error.
+        */
+
+    } else if (WIFEXITED(status)) {
+        /* Child process exited with WEXITSTATUS(status) status.
+         * A status of 0 (or EXIT_SUCCESS) means success,
+         * no errors occurred. Nonzero usually means an error,
+         * but codes vary from binary to binary.
+        */
+
+    } else if (WIFSIGNALED(status)) {
+        /* Child process died from WTERMSIG(status) signal.
+         * If you include <string.h>, you can use
+         *     strsignal(WTERMSIG(status))
+         * to obtain the name (string) of the terminating signal.
+        */
+
+    } else {
+        /* Child process died from unknown causes.
+        */
+
+    }
+
+    /* All done. */
+    return 1;
+}
+
+pid_t hw_spawn(char const *exec_path, char * const *argv)
+{
+    pid_t pid;
+    int status = posix_spawn(&pid, exec_path, NULL, NULL, argv, environ);
+    return status;
+}
+
+int hw_spawn_lock(char const *exec_path, char * const *argv)
+{
+    pid_t pid;
+    int status = posix_spawn(&pid, exec_path, NULL, NULL, argv, environ);
+    if (status == 0) {
+        do {
+            if (waitpid(pid, &status, 0) == -1) {
+                exit(1);
+            }
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return pid;
+}
+
+int hw_cmd_lock(char * const cmd_nullterm)
+{
+    return hw_spawn_lock("/bin/sh"
+                  , HW_NULLTERM_ARRAY("sh", "-c", cmd_nullterm));
+}
+
+pid_t hw_cmd(char * const cmd_nullterm)
+{
+    return hw_spawn("/bin/sh"
+                  , HW_NULLTERM_ARRAY("sh", "-c", cmd_nullterm));
+}
+
+
+
+/**
+ * Section: ALLOCATORS
+ */
 
 HW_DEBUG(
     static thread_local hw_uint total_alloc_size = 0;
